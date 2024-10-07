3f3aae2a2476adaba27e0788ba7caa7fbc9bf7a2
mikewardmckee <mikewardmckee@gmail.com>
2024-10-07 12:29:50 -0700

    Modified index.cshtml.cs to be 3333 in summary

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Mike MCKEE 1111
+    /// Mike MCKEE 3333
     /// </summary>
     public class IndexModel : PageModel
     {
